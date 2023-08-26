ac5383eac983e86da14ddcea8dca220c89b15864
Vashu-Nayak <nvashu1@gmail.com>
2023-08-26 23:13:33 +0530

    1at commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from 'react';
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  const [task, setTask] = useState('');
+
+  const addTodo = () => {
+    if (task.trim() !== '') {
+      setTodos([...todos, task]);
+      setTask('');
+    }
+  };
+
+  const removeTodo = (index) => {
+    const newTodos = todos.filter((_, i) => i !== index);
+    setTodos(newTodos);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 id="header">Todo List</h1>
+      <div>
+        <input id='input-bar'
+          type="text"
+          value={task}
+          onChange={(e) => setTask(e.target.value)}
+          placeholder="Enter a task"
+        />
+        &nbsp;&nbsp;&nbsp;
+        <button id='taskbutton' onClick={addTodo}>Add</button>
+      </div>
+      <ul style={{listStyle:"none"}}>
+        {todos.map((todo, index) => (
+          <li key={index}>
+            {index+1 } &nbsp; &nbsp; {todo} &nbsp; &nbsp;
+            <button onClick={() => removeTodo(index)}>Remove</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
-
 export default App;
